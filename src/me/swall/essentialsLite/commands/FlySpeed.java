f988822e04305a0934cc12ea2d953ffd47136510
swalloich <swalloich@live.com>
2020-09-03 16:27:11 -0600

    Fixed the flyspeed command
    Changed the syntax of the flyspeed command and fixed changing the flyspeed of other players.

--- src/me/swall/essentialsLite/commands/FlySpeed.java
+++ src/me/swall/essentialsLite/commands/FlySpeed.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
 import me.swall.essentialsLite.Main;
 
 public class FlySpeed implements CommandExecutor 
@@ -15,6 +16,20 @@ public class FlySpeed implements CommandExecutor
 	{
 		this.plugin = plugin;
 		plugin.getCommand("flySpeed").setExecutor(this);
+	}
+	
+	private boolean isNumber(String in)
+	{
+		boolean isNumber = true;
+		for (int i = 0; i < in.length(); i++)
+		{
+			char c = in.charAt(i);
+		    if (!Character.isDigit(c))
+			{
+				isNumber = false;
+			}
+		}
+		return isNumber;
 	}
 	
 	@Override
@@ -29,61 +44,37 @@ public class FlySpeed implements CommandExecutor
 		{	
 			Player player = (Player) sender;
 			if (player.hasPermission("essentialsLite.flySpeed"))
-			{				
-				//check the first argument to ensure it is an integer
-				boolean hasExtTarget = false;
-				for (int i = 0; i < args[0].length(); i++)
+			{	
+				if (args.length > 0 && isNumber(args[0]))
 				{
-					if (!Character.isDigit(args[0].charAt(i)))
+					if (args.length == 1)
 					{
-						hasExtTarget = true;
-					}
-				}
-				
-				if (!hasExtTarget)
-				{
-					float flySpeed = (float)(Integer.parseInt(args[0]) / 10.0);
-					if (Integer.parseInt(args[0]) > 10 || Integer.parseInt(args[0]) < 0)
-					{
-						sender.sendMessage("The speed argument must be a whole number between 0 and 10.");
-					}
-					else
-					{
-						//set the sender's fly speed to the value provided as the first argument
+						float flySpeed = (Float.parseFloat(args[0]) / (float)10.0);
 						player.setFlySpeed(flySpeed);
-						sender.sendMessage("Your fly speed was set to " + args[0] + ".");
 					}
-				}
-				else 
-				{
-					float flySpeed = (float)(Integer.parseInt(args[1]) / 10.0);
-					Bukkit.getLogger().info(player.getDisplayName() + " targeted " + args[0] + " with the flySpeed command.");
-					if (Integer.parseInt(args[0]) > 10 || Integer.parseInt(args[0]) < 0)
+					else if (args[1].length() > 0)
 					{
-						sender.sendMessage("The speed argument must be a whole number between 0 and 10.");
-					}
-					else
-					{
-						//set the target player's fly speed to the value provided as the second argument
-						Player targetPlayer = Bukkit.getPlayerExact(args[0]);
-						Bukkit.getLogger().info("targetPlayer was initialized.");
-						if (targetPlayer != null && player.isOnline())
+						float flySpeed = (Float.parseFloat(args[0]) / (float)10.0);
+						Player targetPlayer = Bukkit.getPlayerExact(args[1]);
+						if (targetPlayer == null)
 						{
-							targetPlayer.setFlySpeed(flySpeed);
-							Bukkit.getLogger().info("The fly speed for user " + player.getDisplayName() 
-							+ " has been set to" + args[0]);
+							player.sendMessage("That player could not be found.");
 						}
 						else
 						{
-							sender.sendMessage("That player was not found.");
+							targetPlayer.setFlySpeed(flySpeed);
 						}
 					}
+				}
+				else
+				{
+					player.sendMessage(args[0] + " is not a valid number.");
 				}
 			}
 			else 
 			{
 				player.sendMessage("You do not have permission to use that command!");
-			}
+			}		
 		}
 		return false;
 	}
